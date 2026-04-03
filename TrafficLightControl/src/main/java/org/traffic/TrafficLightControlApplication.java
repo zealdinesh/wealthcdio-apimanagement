3d20d0e6a23e50b8e92c2dcee642d40c6aa48537
Dinesh <zealdinesh@gmail.com>
2026-04-04 02:09:34 +0530

    Traffic Light Control

---
+++ TrafficLightControl/src/main/java/org/traffic/TrafficLightControlApplication.java
@@ -0,0 +1,13 @@
+package org.traffic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficLightControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficLightControlApplication.class, args);
+    }
+
+}
